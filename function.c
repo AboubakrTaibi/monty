b18333386abd1ce07b02c2c090ae5cd79aa23c7b
nisselhbb <nissrineelhobbi@gmail.com>
2023-09-19 12:48:44 -0700

    contains the main function that copies the content of a file to another file

--- function.c
+++ function.c
@@ -1,6 +1,9 @@
 #include "monty.h"
 #include <stdio.h>
 #include <string.h>
+
+globals_var vars;
+
 /**
  * closefile - function that close file stream.
  * @file: file to close
@@ -9,11 +12,11 @@
 void closefile(FILE *file)
 {
 
-if (fclose(file) == EOF)
-{
-fprintf(stderr, "Error: Can't close file\n");
-exit(EXIT_FAILURE);
-}
+	if (fclose(file) == EOF)
+	{
+		fprintf(stderr, "Error: Can't close file\n");
+		exit(EXIT_FAILURE);
+	}
 
 }
 /**
@@ -21,48 +24,39 @@ exit(EXIT_FAILURE);
  * @argc: The number of command-line arguments
  * @argv: An array of strings representing the arguments
  * Return: Always 0.
-  */
+ */
 int main(int argc, char *argv[])
 {
 
-FILE *o_opfile;
-char  *opfile, line[BUF_SIZE], **args;
-ssize_t read = 0;
-int line_count = 0, i;
-stack_t *stack;
-(void)i;
+	char  *opfile, line[BUF_SIZE] = {'\0'};
+	ssize_t read = 0;
+	int line_count = 0, i;
+	stack_t *stack = NULL;
+	(void)i;
 
-if (argc != 2)
-{ fprintf(stderr, "USAGE: monty file\n");
-exit(EXIT_FAILURE); }
+	if (argc != 2)
+	{ fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE); }
 
-opfile = argv[1];
-o_opfile = fopen(opfile, "r");
-if (o_opfile == NULL)
-{ fprintf(stderr, "Error: Can't open file %s\n", opfile);
-exit(EXIT_FAILURE); }
+	opfile = argv[1];
+	vars.o_opfile = fopen(opfile, "r");
+	if (vars.o_opfile == NULL)
+	{ fprintf(stderr, "Error: Can't open file %s\n", opfile);
+		exit(EXIT_FAILURE); }
 
-while (fgets(line, sizeof(line), o_opfile) != NULL)
-{
-line_count++;
+	while (fgets(line, sizeof(line), vars.o_opfile) != NULL)
+	{
+		line_count++;
+		vars.args = strtok_alloc(line, read);
+		if (!vars.args || vars.args[0][0] == '#')
+			continue;
+		if (strcmp(vars.args[0], "push") == 0)
+			vars.number = _atoi(vars.args[1], stack, line_count);
 
-args = strtok_alloc(line, read);
-if (!args || args[0][0] == '#')
-    continue;
-if (strcmp(args[0], "push") == 0)
-{
-if (!args[1] || !_atoi(args[1]))
-    { print_error(line_count, ": usage: push integer", NULL);
-    free(args);
-
-    exit(EXIT_FAILURE); }
-
-}
-
-execute_opcode(&stack, args[0], line_count);
-free(args);
-}
-
-closefile(o_opfile);
-return (0);
+		execute_opcode(&stack, vars.args[0], line_count);
+		free(vars.args);
+	}
+	free_stack(stack);
+	closefile(vars.o_opfile);
+	return (0);
 }
