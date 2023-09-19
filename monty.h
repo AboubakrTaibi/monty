da3ab6ed936902e93061186837e0d849334b5388
aboubakr <c.aboubakr@gmail.com>
2023-09-19 21:19:39 +0100

    Added 'struct global_var' containing global variables.

--- monty.h
+++ monty.h
@@ -5,10 +5,9 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <errno.h>
-#include <fcntl.h> 
+#include <fcntl.h>
 #include <string.h>
-
-
+#define BUF_SIZE 1024
 
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
@@ -21,9 +20,9 @@
  */
 typedef struct stack_s
 {
-    int n;
-    struct stack_s *prev;
-    struct stack_s *next;
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
 } stack_t;
 
 /**
@@ -36,15 +35,29 @@ typedef struct stack_s
  */
 typedef struct instruction_s
 {
-    char *opcode;
-    void (*f)(stack_t **stack, unsigned int line_number);
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
+/**
+ * struct global_var - global vars used in this project
+ * @number: integer number to add to stack
+ * @args: pointer to pointer
+ *@o_opfile: file stream
+ */
+typedef struct global_var
+{
+int number;
+char **args;
+FILE *o_opfile;
+} globals_var;
+
+extern globals_var vars;
 
 void closefile(FILE *file);
 char **strtok_alloc(char *line, ssize_t read);
 void print_number(int std, int n);
 int _puts(char *c);
-int _atoi(char *s);
+int _atoi(char *argument, stack_t *stack, int line_count);
 int _puts_std(int std, char *str);
 int _slen(char *s);
 int p_err_getline(void);
@@ -56,6 +69,8 @@ void pall_func(stack_t **stack, unsigned int line_number);
 void pint_func(stack_t **stack, unsigned int line_number);
 void pop_func(stack_t **stack, unsigned int line_number);
 char *_strdup(const char *str);
+void free_stack(stack_t *stack);
+
 
 
 #endif /* MONTY_H */
