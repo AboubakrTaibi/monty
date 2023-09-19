8b737919d8017c8f124a15bc9e37d7dd7920da62
aboubakr <c.aboubakr@gmail.com>
2023-09-19 15:16:43 +0100

    Implement _strdup.c: Adds a file for the _strdup function, which duplicates a given string and returns a pointer to the newly allocated copy.

---
+++ _strdup.c
@@ -0,0 +1,28 @@
+#include "monty.h"
+
+/**
+ * *_strdup -  function that returns a pointer to a newly allocated space
+ * in memory, which contains a copy of the string given as a parameter
+ * returns a pointer to a new string which is a duplicate of the string str
+ * @str: pointer to a string
+ * Return: a pointer to duplicated string., or NULL if it fails
+ */
+char *_strdup(const char *str)
+{
+int i = 0;
+char *pdupstr = NULL;
+if (str == NULL)
+return (NULL);
+
+while (str[i])
+i++;
+
+pdupstr = (char *)malloc((i + 1) * sizeof(char));
+if (pdupstr == NULL)
+return (NULL);
+
+for (i = 0; str[i]; i++)
+pdupstr[i] = str[i];
+pdupstr[i] = '\0';
+return (pdupstr);
+}
