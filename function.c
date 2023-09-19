18ebede09e2a4fa4a0698832b7bcb21197a461f0
nisselhbb <nissrineelhobbi@gmail.com>
2023-09-19 07:33:32 -0700

    contains the main function

--- function.c
+++ function.c
@@ -26,7 +26,7 @@ int main(int argc, char *argv[])
 {
 
 FILE *o_opfile;
-char  *opfile, *line = NULL, **args;
+char  *opfile, line[BUF_SIZE], **args;
 ssize_t read = 0;
 int line_count = 0, i;
 stack_t *stack;
@@ -42,9 +42,10 @@ if (o_opfile == NULL)
 { fprintf(stderr, "Error: Can't open file %s\n", opfile);
 exit(EXIT_FAILURE); }
 
-while (fgets(line, 100, o_opfile) != NULL)
+while (fgets(line, sizeof(line), o_opfile) != NULL)
 {
 line_count++;
+
 args = strtok_alloc(line, read);
 if (!args || args[0][0] == '#')
     continue;
@@ -53,17 +54,15 @@ if (strcmp(args[0], "push") == 0)
 if (!args[1] || !_atoi(args[1]))
     { print_error(line_count, ": usage: push integer", NULL);
     free(args);
-    free(line);
+
     exit(EXIT_FAILURE); }
 
 }
 
 execute_opcode(&stack, args[0], line_count);
-free(line);
 free(args);
-line = NULL;
 }
-free(line);
+
 closefile(o_opfile);
 return (0);
 }
