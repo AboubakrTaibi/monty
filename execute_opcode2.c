fba7c81c3357ee1e38b76c85e985f63ae22b50f9
nisselhbb <nissrineelhobbi@gmail.com>
2023-09-20 05:38:46 -0700

    added the mod function that computes the rest of the division of the second top element of the stack by the top element of the stack

--- execute_opcode2.c
+++ execute_opcode2.c
@@ -44,3 +44,30 @@ pop_func(stack, line_number);
 }
 }
 
+/**
+ * mod_func - function that computes the rest of the division of the second
+ * top element of the stack by the top element of the stack.
+ * @stack: pointer to stack list
+ * @line_number: number of line in file
+ */
+void mod_func(stack_t **stack,  unsigned int line_number)
+{
+
+if (stack == NULL)
+    exit(EXIT_FAILURE);
+else if (!(*stack) || !((*stack)->next))
+    { fprintf(stderr, "L%d: can't mod, stack too short\n", line_number);
+    free_all(stack);
+    exit(EXIT_FAILURE); }
+
+else if (!(*stack)->n)
+    { fprintf(stderr, "L%d: division by zero\n", line_number);
+    free_all(stack);
+    exit(EXIT_FAILURE); }
+else
+{
+(*stack)->next->n %= (*stack)->n;
+pop_func(stack, line_number);
+}
+}
+
