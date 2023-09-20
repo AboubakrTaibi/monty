633bb3bc83b5afbac1b342e3a2967213d74e2728
nisselhbb <nissrineelhobbi@gmail.com>
2023-09-20 05:17:22 -0700

    added the sub function that subtracts the top element of the stack from the second top element of the stack

--- execute_opcode1.c
+++ execute_opcode1.c
@@ -26,21 +26,26 @@ void swap_func(stack_t **stack, unsigned int line_number)
 		(*stack)->next->n = tmp;
 	}
 }
-/**
- * sub_func - function that subtracts the top element of the stack from the
- * second top element of the stack.
- * @stack: pointer to stack list
- * @line_number: number of line in file
- */
-void sub_func(stack_t **stack,  unsigned int line_number)
-{
-if (stack == NULL)
-	exit(EXIT_FAILURE);
-if (!(*stack) || !((*stack)->next))
-	{ fprintf(stderr, "L%d: can't sub, stack too short", line_number);
-	free_all(stack);
-	exit(EXIT_FAILURE); }
-(*stack)->next->n -= (*stack)->n;
-pop_func(stack, line_number);
-}
 
+/**
+ * sub_func - subtracts the top element from
+ * the second top element of the stack
+ * @stack: pointer to the top of the stack
+ * @line_number: line number of the instruction
+ */
+void sub_func(stack_t **stack, unsigned int line_number)
+{
+        if (stack == NULL)
+                exit(EXIT_FAILURE);
+    else if (!(*stack) || !(*stack)->next)
+    {
+            fprintf(stderr, "L%u: can't sub, stack too short\n", line_number);
+            free_all(stack);
+            exit(EXIT_FAILURE);
+    }
+    else
+    {
+            (*stack)->next->n -= (*stack)->n;
+            pop_func(stack, line_number);
+    }
+}
