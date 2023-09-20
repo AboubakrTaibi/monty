a2378e5cc8cd0bf7748d4d06ccc1d24996d7741d
nisselhbb <nissrineelhobbi@gmail.com>
2023-09-20 04:45:37 -0700

    added the swap function that swaps the top two elements of the stack

--- execute_opcode1.c
+++ execute_opcode1.c
@@ -1,21 +1,30 @@
 #include "monty.h"
-
 /**
- * swap_func - function that adds the top two elements of the stack.
- * @stack: pointer to stack list
- * @line_number: number of line in file
+ * swap_func - swaps the top two elements of the stack
+ * @stack: pointer to the top of the stack
+ * @line_number: line number of the instruction
  */
-void swap_func(stack_t **stack,  unsigned int line_number)
+void swap_func(stack_t **stack, unsigned int line_number)
 {
+	int tmp;
+	(void)line_number;
 
-if (stack == NULL)
-	exit(EXIT_FAILURE);
-if (!(*stack) || !((*stack)->next))
-	{ fprintf(stderr, "L%d: can't add, stack too short", line_number);
-	free_all(stack);
-	exit(EXIT_FAILURE); }
-(*stack)->next->n += (*stack)->n;
-pop_func(stack, line_number);
+	if (stack == NULL)
+	{
+		exit(EXIT_FAILURE);
+	}
+	else if (!(*stack) || !(*stack)->next)
+	{
+		fprintf(stderr, "L%u: can't swap, stack too short\n", line_number);
+		free_all(stack);
+		exit(EXIT_FAILURE);
+	}
+	else
+	{
+		tmp = (*stack)->n;
+		(*stack)->n = (*stack)->next->n;
+		(*stack)->next->n = tmp;
+	}
 }
 /**
  * sub_func - function that subtracts the top element of the stack from the
